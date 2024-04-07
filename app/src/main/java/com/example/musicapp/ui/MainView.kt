1680fb9049e30a06de1332cb492eac2801de489c
KrishKoria <krishkoria2004@gmail.com>
2024-04-07 18:48:01 +0530

    Added Side Drawer

--- app/src/main/java/com/example/musicapp/ui/MainView.kt
+++ app/src/main/java/com/example/musicapp/ui/MainView.kt
@@ -1,6 +1,8 @@
 package com.example.musicapp.ui
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.DrawerState
@@ -8,13 +10,22 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.musicapp.Screens
 import kotlinx.coroutines.launch
 
 
@@ -22,26 +33,61 @@ import kotlinx.coroutines.launch
 @Composable
 fun MainView() {
     val drawerState = remember { DrawerState(DrawerValue.Closed) }
-//    val snackbarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
-    Scaffold(
-//        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
-        topBar = {
-            TopAppBar(title = { Text(text = "Hello") },
-                navigationIcon = {
-                    IconButton(onClick = {
-                        scope.launch {
-                            drawerState.open()
+    val screensInDrawer = listOf(
+        Screens.DrawerScreen.Account,
+        Screens.DrawerScreen.Subscription,
+        Screens.DrawerScreen.AddAccount
+    )
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val title = remember { mutableStateOf("") }
+    ModalNavigationDrawer(drawerContent = {
+        ModalDrawerSheet {
+            LazyColumn(Modifier.padding(16.dp)) {
+                items(screensInDrawer) { screen ->
+                    NavigationDrawerItem(
+                        label = { Text(screen.title) },
+                        selected = currentRoute == screen.route,
+                        onClick = {
+                            scope.launch {
+                                drawerState.close()
+
+                            }
+                            if (screen.dRoute == "add_account") {
+                                //dialog
+                            } else {
+                                navController.navigate(screen.dRoute)
+                                title.value = screen.dTitle
+                            }
                         }
-                    }) {
-                        Icon(
-                            imageVector = Icons.Default.AccountCircle,
-                            contentDescription = "Menu"
-                        )
-                    }
-                })
+                    )
+                }
+            }
         }
-    ) {
-        Text(text = "Hello, MusicApp!", modifier = Modifier.padding(it))
+    }, drawerState = drawerState, gesturesEnabled = true) {
+        Scaffold(
+            topBar = {
+                TopAppBar(title = { Text(text = "Hello") },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            scope.launch {
+                                drawerState.apply {
+                                    if (isClosed) open() else close()
+                                }
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.Default.AccountCircle,
+                                contentDescription = "Menu"
+                            )
+                        }
+                    })
+            },
+        ) {
+            Text(text = "Hello, MusicApp!", modifier = Modifier.padding(it))
+        }
     }
 }
